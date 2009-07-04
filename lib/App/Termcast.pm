3673f0638493b84e0f30149987c246e85a403068
Jesse Luehrs <doy@tozt.net>
2009-07-03 22:19:01 -0500

    initial commit

---
+++ lib/App/Termcast.pm
@@ -0,0 +1,71 @@
+package App::Termcast;
+use Moose;
+
+=head1 NAME
+
+App::Termcast -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-app-termcast at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Termcast>.
+
+=head1 SEE ALSO
+
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc App::Termcast
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-Termcast>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-Termcast>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Termcast>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-Termcast>
+
+=back
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
