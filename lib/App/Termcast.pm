9d99f3ccfd4b4aa83fa09d6beaa0589c01eecbda
Jesse Luehrs <doy@tozt.net>
2011-04-14 09:05:31 -0500

    stop using the weaver template thing

--- lib/App/Termcast.pm
+++ lib/App/Termcast.pm
@@ -420,9 +420,45 @@ no Moose;
 
 Use L<MooseX::SimpleConfig> to make configuration easier.
 
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-app-termcast at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Termcast>.
+
 =head1 SEE ALSO
 
 L<http://termcast.org/>
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc App::Termcast
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-Termcast>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-Termcast>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Termcast>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-Termcast>
+
+=back
 
 =cut
 
